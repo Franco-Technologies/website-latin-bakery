e2a6b4809af6a642e49e6f8994f0ca9310866757
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-11-15 21:01:24 -0600

    Add carousel functionality and notification close feature

---
+++ content/script.js
@@ -0,0 +1,44 @@
+let slideIndex = 0;
+
+function showSlides() {
+  let i;
+  let slides = document.querySelectorAll(".carousel-images img");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  let id = setTimeout(showSlides, 4000); // Change image every 4 seconds
+
+  // If the user clicks on the next or previous button, clear the timeout
+  document.querySelectorAll(".prev, .next").forEach((button) => {
+    button.addEventListener("click", () => {
+      clearTimeout(id);
+    });
+  });
+}
+
+function moveSlide(n) {
+  slideIndex += n - 1;
+  //   clear the interval
+  showSlides();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSlides(slideIndex);
+});
+
+// close notification
+document.addEventListener("DOMContentLoaded", () => {
+  const closeBtn = document.querySelector("#closeNotification");
+  closeBtn.addEventListener("click", () => {
+    const notification = document.querySelector("#notification");
+    notification.style.display = "none";
+  });
+});
